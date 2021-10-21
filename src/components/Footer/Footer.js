a5c2c40c4059c06574d53898192d8a2da53cdfdd
toxicmuffin <toxicmuffin101@hotmail.com>
2021-10-20 22:41:21 -0500

    figuring out the best way set my footer data

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,62 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 //data
+// There are two ways I can approach this...
+// I can use an array of objects and break them up by categor
+const FooterHeadings = [
+    {
+        text: 'Heading One'
+    },
+    {
+        text: 'Heading Two'
+    }
+]
+const FooterItems = [
+    {
+        text: 'home',
+        link: '/',
+    },
+    {
+        text: 'lorem',
+        link: '/'
+    }
+]
+
+// or
+// I can use an array of objects of objects...?
+const FooterItemsV2 = [
+    {
+        grouping: {
+            heading: {
+                text: 'Heading One',
+            },
+            item1: {
+                text: 'item 1',
+                link: '/item1',
+            },
+            item2: {
+                text: 'item 2',
+                link: '/item2',
+            }
+        },
+        grouping: {
+            headings: 'Heading Two',
+        },
+        item1: {
+            text: 'item one',
+            link: '/itemOne',
+        },
+        item2: {
+            text: 'item two',
+            link: '/itemTwo',
+        }
+    }
+]
 
 const Footer = () => {
     return (
         <footer>
-            
+
         </footer>
     )
 }
