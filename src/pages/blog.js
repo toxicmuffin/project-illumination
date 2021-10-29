00387d9e5e464f4da584b33eb2390b57fe28838d
toxicmuffin <toxicmuffin101@hotmail.com>
2021-10-28 20:14:33 -0500

    reference sources

---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+//https://github.com/gatsbyjs/gatsby-starter-blog/blob/master/src/templates/blog-post.js
+
+const BlogPostTemplate = () => {
+    
+    return (
+        <layout>
+
+        </layout>
+    )
+}
+
+export default BlogPostTemplate
